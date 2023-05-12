b7a82a5a55ea4a7b5750e5218bbddcb0fe0cb62c
pb4git <pbpubpub@gmail.com>
2023-05-12 18:38:05 +0200

    Run 36: Based on run 35. Go back to 1 step, increase epsilon to 5%, disable the removal of w_reward after 5 hours

--- scripts/train.py
+++ scripts/train.py
@@ -17,7 +17,7 @@ from trackmania_rl.experience_replay.basic_experience_replay import BasicExperie
 
 base_dir = Path(__file__).resolve().parents[1]
 
-run_name = "34"
+run_name = "36"
 map_name = "map3"
 zone_centers = np.load(str(base_dir / "maps" / f"{map_name}_{misc.distance_between_checkpoints}m.npy"))
 
@@ -298,21 +298,21 @@ while True:
         tag="explo_race_time",
         scalar_value=fast_stats_tracker["race_time"][-1] / 1000,
         global_step=cumul_number_frames_played,
-        walltime=float(cumul_training_hours * 3600),
+        walltime=float(cumul_training_hours * 3600) + time.time() - (time_next_save - misc.statistics_save_period_seconds),
     )
     if fast_stats_tracker["race_finished"][-1]:
         tensorboard_writer.add_scalar(
             tag="explo_race_time_finished",
             scalar_value=fast_stats_tracker["race_time"][-1] / 1000,
             global_step=cumul_number_frames_played,
-            walltime=float(cumul_training_hours * 3600),
+            walltime=float(cumul_training_hours * 3600) + time.time() - (time_next_save - misc.statistics_save_period_seconds),
         )
 
     tensorboard_writer.add_scalar(
         tag="single_zone_reached",
         scalar_value=fast_stats_tracker["zone_reached"][-1],
         global_step=cumul_number_frames_played,
-        walltime=float(cumul_training_hours * 3600),
+        walltime=float(cumul_training_hours * 3600) + time.time() - (time_next_save - misc.statistics_save_period_seconds),
     )
     print("race time ratio  ", np.median(np.array(fast_stats_tracker["race_time_ratio"])))
     (
@@ -505,7 +505,7 @@ while True:
                 tag="eval_race_time_finished",
                 scalar_value=eval_stats_tracker["race_time"][-1] / 1000,
                 global_step=cumul_number_frames_played,
-                walltime=float(cumul_training_hours * 3600),
+                walltime=float(cumul_training_hours * 3600) + time.time() - (time_next_save - misc.statistics_save_period_seconds),
             )
 
         print("EVAL EVAL EVAL EVAL EVAL EVAL EVAL EVAL EVAL EVAL")
@@ -640,8 +640,8 @@ while True:
         # ===============================================
         importlib.reload(misc)
 
-        if cumul_training_hours > 2:
-            misc.reward_per_ms_press_forward = 0
+        # if cumul_training_hours > 2:
+        #     misc.reward_per_ms_press_forward = 0
 
         time_next_save += misc.statistics_save_period_seconds
 
--- trackmania_rl/misc.py
+++ trackmania_rl/misc.py
@@ -13,14 +13,14 @@ n_zone_centers_in_inputs = 16
 max_overall_duration_ms = 180_000
 max_minirace_duration_ms = 25_000
 
-epsilon = 0.01
+epsilon = 0.05
 q_values_randomization_amplitude = 0.02
 discard_non_greedy_actions_in_nsteps = True
 
 anneal_step = 1
 n_steps = [
     1,
-    3,
+    1,
     3,
     3,
     3,
@@ -86,7 +86,7 @@ memory_size = 300_000
 memory_size_start_learn = 10000
 virtual_memory_size_start_learn = 1000
 number_memories_generated_high_exploration = 100000
-high_exploration_ratio = 25
+high_exploration_ratio = 5
 batch_size = 1024
 learning_rate = 5e-5
 
