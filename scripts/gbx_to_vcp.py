1e4cec152e3d2173e18e4dc880d22581d998ca06
pb4git <pb.pubpub@gmail.com>
2023-09-15 21:48:38 +0200

    Modified VCP extraction from GBX such that the best ghost is taken if several are present.

--- scripts/gbx_to_vcp.py
+++ scripts/gbx_to_vcp.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import numpy as np
@@ -5,18 +6,43 @@ from pygbx import Gbx, GbxType
 
 from trackmania_rl.geometry import extract_cp_distance_interval
 
-gbx = Gbx("A01-Race_eddieman194(00'23''79).Replay.gbx")
-ghost = gbx.get_class_by_id(GbxType.CTN_GHOST)
-if not ghost:
-    exit()
-if ghost.num_respawns > 0:
-    print("WARNING: GHOST HAS RESPAWNS")
 
-print(ghost.race_time, f"ghost has {len(ghost.records)} records and {len(ghost.control_entries)} control entries")
-records_to_keep = round(ghost.race_time / 100)
-print("Keeping", records_to_keep, "out of", len(ghost.records), "records for a race time of", ghost.race_time / 1000)
-raw_positions_list = []
-for r in ghost.records[:records_to_keep]:
-    raw_positions_list.append(np.array([r.position.x, r.position.y, r.position.z]))
+def gbx_to_vcp(gbx_path: str, base_dir):
+    gbx = Gbx(gbx_path)
+    ghosts = gbx.get_classes_by_ids([GbxType.CTN_GHOST])
+    assert len(ghosts) > 0, "The file does not contain any ghost."
+    ghost = min(ghosts, key=lambda ghost: ghost.cp_times[-1])
+    assert ghost.num_respawns == 0, "The ghost contains respawns"
+    print(ghost.race_time, f"ghost has {len(ghost.records)} records and {len(ghost.control_entries)} control entries")
+    records_to_keep = round(ghost.race_time / 100)
+    print("Keeping", records_to_keep, "out of", len(ghost.records), "records for a race time of", ghost.race_time / 1000)
+    raw_positions_list = []
+    for r in ghost.records[:records_to_keep]:
+        raw_positions_list.append(np.array([r.position.x, r.position.y, r.position.z]))
+    _ = extract_cp_distance_interval(raw_positions_list, 0.5, base_dir)
+
+
 base_dir = Path(__file__).resolve().parents[1]
-_ = extract_cp_distance_interval(raw_positions_list, 0.5, base_dir)
+
+"""
+EXTRACT VCP FROM A SINGLE GBX
+"""
+gbx_to_vcp("Map5_pb4608(02'04''91).Replay.Gbx", base_dir)
+
+
+"""
+EXTRACT VCP FROM MULTIPLE GBX
+"""
+# replay_dir = Path(r"C:\Users\chopi\Documents\TrackMania\Tracks\Challenges\Official Maps\Green\replays")
+
+# for replay_file in replay_dir.iterdir():
+#     map_name = replay_file.name.split(".")[0]
+#     # gbx_to_vcp(str(replay_file), base_dir)
+#     # shutil.copyfile(base_dir / "maps" / "map.npy", base_dir / "maps" / f"{map_name}_0.5m_author.npy")
+
+"""
+LIST MAP NAMES
+"""
+# [replay_file.name.split('.')[0] for replay_file in replay_dir.iterdir()]
+
+
