363c12489420be0a16d131c368f53d83eaa64e89
pb4git <pb.pubpub@gmail.com>
2023-10-15 15:16:38 +0200

    Added risk sensitive curves in A and Q curves.

--- trackmania_rl/temporary_crap.py
+++ trackmania_rl/temporary_crap.py
@@ -7,6 +7,21 @@ from . import misc
 
 
 def race_time_left_curves(rollout_results, trainer, save_dir):
+    color_cycle = [
+        "red",
+        "forestgreen",
+        "blue",
+        "darkred",
+        "darkgreen",
+        "darkblue",
+        "salmon",
+        "limegreen",
+        "cornflowerblue",
+        "orange",
+        "lime",
+        "cyan",
+    ]
+
     rollout_results_copy = rollout_results.copy()
     for frame_number in [0, 5, 10, 20]:
         for x_axis in [
@@ -15,6 +30,10 @@ def race_time_left_curves(rollout_results, trainer, save_dir):
         ]:
             q = defaultdict(list)
             a = defaultdict(list)
+            q_l = defaultdict(list)
+            a_l = defaultdict(list)
+            q_h = defaultdict(list)
+            a_h = defaultdict(list)
 
             tau = torch.linspace(0.05, 0.95, misc.iqn_k)[:, None].to("cuda")
             for j in x_axis:
@@ -27,16 +46,30 @@ def race_time_left_curves(rollout_results, trainer, save_dir):
                     # print(i, q_val)
                     q[i].append(q_val)
                     a[i].append(q_val - per_quantile_output.mean(axis=0).max())
-                print(j, per_quantile_output.mean(axis=0).argmax())
 
-            for i in range(12):
-                plt.plot(x_axis, a[i], label=str(i))
+                for i, q_val in enumerate(list(per_quantile_output[: misc.iqn_k // 2, :].mean(axis=0))):
+                    # print(i, q_val)
+                    q_l[i].append(q_val)
+                    a_l[i].append(q_val - per_quantile_output[: misc.iqn_k // 2, :].mean(axis=0).max())
+
+                for i, q_val in enumerate(list(per_quantile_output[misc.iqn_k // 2 :, :].mean(axis=0))):
+                    # print(i, q_val)
+                    q_h[i].append(q_val)
+                    a_h[i].append(q_val - per_quantile_output[misc.iqn_k // 2 :, :].mean(axis=0).max())
+
+            for i in reversed(range(12)):
+                plt.plot(x_axis, a_l[i], label=str(i) + "_l", c=color_cycle[i], linestyle="dotted")
+                plt.plot(x_axis, a_h[i], label=str(i) + "_h", c=color_cycle[i], linestyle="dashed")
+            for i in reversed(range(12)):
+                plt.plot(x_axis, a[i], label=str(i), c=color_cycle[i])
             plt.gcf().legend()
-            plt.savefig(save_dir / f"crap_A_{str(x_axis)}_{frame_number}.png")
+            plt.gcf().suptitle(f"crap_A_{str(x_axis)[5:]}_{frame_number}.png")
+            plt.savefig(save_dir / f"crap_A_{str(x_axis)[5:]}_{frame_number}.png")
             plt.close()
 
-            for i in range(12):
-                plt.plot(x_axis, q[i], label=str(i))
+            for i in reversed(range(12)):
+                plt.plot(x_axis, q[i], label=str(i), c=color_cycle[i])
             plt.gcf().legend()
-            plt.savefig(save_dir / f"crap_Q_{str(x_axis)}_{frame_number}.png")
+            plt.gcf().suptitle(f"crap_Q_{str(x_axis)[5:]}_{frame_number}.png")
+            plt.savefig(save_dir / f"crap_Q_{str(x_axis)[5:]}_{frame_number}.png")
             plt.close()
