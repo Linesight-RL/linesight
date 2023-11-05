896add873b4d0ac0120c20727ba0e384c8c8cac7
pb4git <pb.pubpub@gmail.com>
2023-11-05 23:26:26 +0100

    Fix RandomCrop which cropped too much.

--- trackmania_rl/buffer_utilities.py
+++ trackmania_rl/buffer_utilities.py
@@ -92,7 +92,7 @@ def buffer_collate_function(batch):
     if misc.apply_randomcrop_augmentation:
         # Same transformation is applied for state and next_state.
         # Different transformation is applied to each element in a batch.
-        i, j, h, w = transforms.RandomCrop.get_params(state_img, output_size=misc.cropped_size)
+        i, j, h, w = transforms.RandomCrop.get_params(state_img, output_size=(misc.H_downsized, misc.W_downsized))
         state_img = transforms.functional.crop(
             transforms.functional.pad(state_img, padding=misc.n_pixels_to_crop_on_each_side, padding_mode="edge"), i, j, h, w
         )
