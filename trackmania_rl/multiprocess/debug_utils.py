09293f9b7867148b8749f24a71566b00fa0d3f9b
pb4git <pb.pubpub@gmail.com>
2023-12-04 21:24:12 +0100

    ForkablePdb allows using pdb within a forked process.

---
+++ trackmania_rl/multiprocess/debug_utils.py
@@ -0,0 +1,22 @@
+import os
+import pdb
+import sys
+
+
+# from https://stackoverflow.com/questions/4716533/how-to-attach-debugger-to-a-python-subproccess/23654936#23654936
+class ForkablePdb(pdb.Pdb):
+    _original_stdin_fd = sys.stdin.fileno()
+    _original_stdin = None
+
+    def __init__(self):
+        pdb.Pdb.__init__(self, nosigint=True)
+
+    def _cmdloop(self):
+        current_stdin = sys.stdin
+        try:
+            if not self._original_stdin:
+                self._original_stdin = os.fdopen(self._original_stdin_fd)
+            sys.stdin = self._original_stdin
+            self.cmdloop()
+        finally:
+            sys.stdin = current_stdin
