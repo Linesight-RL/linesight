8ea1b0f416c90f7f63aa525b806be9fd1fd70bea
pb4git <pb.pubpub@gmail.com>
2024-01-13 11:05:05 +0100

    Fix copy of misc to misc_copy that was done too late. Script did not work if misc_copy did not exist at the beginning.

--- scripts/train.py
+++ scripts/train.py
@@ -1,11 +1,40 @@
+import shutil
+from pathlib import Path
+
+
+def copy_configuration_file():
+    # At the beginning of training, parameters are read from misc.py.
+    # They are then copied from misc.py to misc_reloaded_copy.py.
+    # During training, misc_reloaded_copy.py will be reloaded at regular time intervals.
+    #
+    # Training parameters modifications made during training in misc_reloaded_copy.py will be applied on the fly
+    # without losing the existing content of the replay buffer.
+    #
+    # The content of main.py may be modified after starting a run: it will have no effect on the ongoing run.
+    # This setup provides the possibility to:
+    #   1) Modify training parameters on the fly
+    #   2) Continue to code and modify misc.py without impacting an ongoing run.
+
+    base_dir = Path(__file__).resolve().parents[1]
+    shutil.copyfile(
+        base_dir / "trackmania_rl" / "misc.py",
+        base_dir / "trackmania_rl" / "misc_copy.py",
+    )
+
+
+if __name__ == "__main__":
+    copy_configuration_file()
+
+# =======================================================================================================================
+# Actual start of train.py, after copying misc.py
+# =======================================================================================================================
+
 import ctypes
 import os
 import random
-import shutil
 import signal
 import sys
 import time
-from pathlib import Path
 
 import numpy as np
 import torch
@@ -13,7 +42,7 @@ import torch.multiprocessing as mp
 from art import tprint
 from torch.multiprocessing import Lock
 
-from trackmania_rl import misc
+from trackmania_rl import misc_copy
 from trackmania_rl.agents.iqn import make_untrained_iqn_network
 from trackmania_rl.multiprocess.collector_process import collector_process_fn
 from trackmania_rl.multiprocess.learner_process import learner_process_fn
@@ -41,7 +70,7 @@ def signal_handler(sig, frame):
 
 
 def clear_tm_instances():
-    if misc.is_linux:
+    if misc_copy.is_linux:
         os.system("pkill -9 TmForever.exe")
     else:
         os.system("taskkill TmForever.exe /F")
@@ -53,50 +82,33 @@ if __name__ == "__main__":
     clear_tm_instances()
 
     base_dir = Path(__file__).resolve().parents[1]
-    save_dir = base_dir / "save" / misc.run_name
+    save_dir = base_dir / "save" / misc_copy.run_name
     save_dir.mkdir(parents=True, exist_ok=True)
     tensorboard_base_dir = base_dir / "tensorboard"
 
-    # At the beginning of training, parameters are read from misc.py.
-    # They are then copied from misc.py to misc_reloaded_copy.py.
-    # During training, misc_reloaded_copy.py will be reloaded at regular time intervals.
-    #
-    # Training parameters modifications made during training in misc_reloaded_copy.py will be applied on the fly
-    # without losing the existing content of the replay buffer.
-    #
-    # The content of main.py may be modified after starting a run: it will have no effect on the ongoing run.
-    # This setup provides the possibility to:
-    #   1) Modify training parameters on the fly
-    #   2) Continue to code and modify misc.py without impacting an ongoing run.
-
-    shutil.copyfile(
-        base_dir / "trackmania_rl" / "misc.py",
-        base_dir / "trackmania_rl" / "misc_copy.py",
-    )
     # Copy Angelscript plugin to TMInterface dir
     shutil.copyfile(
         base_dir / "trackmania_rl" / "tmi_interaction" / "Python_Link.as",
-        misc.target_python_link_path,
+        misc_copy.target_python_link_path,
     )
 
-    print("Run:")
-    print("\n" * 2)
-    tprint(misc.run_name, font="tarty4")
+    print("Run:\n\n")
+    tprint(misc_copy.run_name, font="tarty4")
     print("\n" * 2)
     tprint("Linesight", font="tarty1")
     print("\n" * 2)
     print("Training is starting!")
 
-    if misc.is_linux:
-        os.system(f"chmod +x {misc.linux_launch_game_path}")
+    if misc_copy.is_linux:
+        os.system(f"chmod +x {misc_copy.linux_launch_game_path}")
 
     # Prepare multi process utilities
     shared_steps = mp.Value(ctypes.c_int64)
     shared_steps.value = 0
-    rollout_queues = [mp.Queue(misc.max_rollout_queue_size) for _ in range(misc.gpu_collectors_count)]
+    rollout_queues = [mp.Queue(misc_copy.max_rollout_queue_size) for _ in range(misc_copy.gpu_collectors_count)]
     shared_network_lock = Lock()
     game_spawning_lock = Lock()
-    _, uncompiled_shared_network = make_untrained_iqn_network(jit=misc.use_jit)
+    _, uncompiled_shared_network = make_untrained_iqn_network(jit=misc_copy.use_jit)
     uncompiled_shared_network.share_memory()
 
     # Start learner process
@@ -128,10 +140,10 @@ if __name__ == "__main__":
                 shared_steps,
                 base_dir,
                 save_dir,
-                misc.base_tmi_port + process_number,
+                misc_copy.base_tmi_port + process_number,
             ),
         )
-        for rollout_queue, process_number in zip(rollout_queues, range(misc.gpu_collectors_count))
+        for rollout_queue, process_number in zip(rollout_queues, range(misc_copy.gpu_collectors_count))
     ]
     for collector_process in collector_processes:
         collector_process.start()
