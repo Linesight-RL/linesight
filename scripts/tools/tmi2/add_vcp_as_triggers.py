853a33b8a423e27cfa42f8b2b4cac421068809fc
pb4git <pb.pubpub@gmail.com>
2024-01-19 19:30:41 +0100

    Refactor add_vcp_as_triggers.py such that the .npy file path may now be passed as an argument, as well as the port on which to connect to the TMI instance. Add a small script docstring at the top.
    (cherry picked from commit 336910e39510861778e149dfdbccecaf3e6c951e)

--- scripts/tools/tmi2/add_vcp_as_triggers.py
+++ scripts/tools/tmi2/add_vcp_as_triggers.py
@@ -1,62 +1,74 @@
-import time
+"""
+This script reads a .npy file containing a list of VCP, and connects to a TMInterface instance to add the VCP as triggers.
+
+This script would typically be used to check that the .npy file contains Virtual CheckPoins (VCP) that are properly placed.
+"""
+import argparse
 from pathlib import Path
 
 import numpy as np
 
 from trackmania_rl.tmi_interaction.tminterface2 import MessageType, TMInterface
 
-iface = TMInterface(8477)
-base_dir = Path(__file__).resolve().parents[3]
-vcp = np.load(base_dir / "maps" / "D15-Endurance_0.5m_gwenlap3.npy")
 
-if not iface.registered:
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("npy_path", type=Path)
+    parser.add_argument("--tmi_port", "-p", type=int, default=8477)
+    args = parser.parse_args()
+
+    iface = TMInterface(args.tmi_port)
+    vcp = np.load(args.npy_path)
+
+    if not iface.registered:
+        while True:
+            try:
+                iface.register(2)
+                break
+            except ConnectionRefusedError as e:
+                print(e)
+
     while True:
-        try:
-            iface.register(2)
-            break
-        except ConnectionRefusedError as e:
-            print(e)
+        msgtype = iface._read_int32()
+        # =============================================
+        #        READ INCOMING MESSAGES
+        # =============================================
+        if msgtype == int(MessageType.SC_RUN_STEP_SYNC):
+            _time = iface._read_int32()
+            # ============================
+            # BEGIN ON RUN STEP
+            # ============================
+            # ============================
+            # END ON RUN STEP
+            # ============================
+            iface._respond_to_call(msgtype)
+        elif msgtype == int(MessageType.SC_CHECKPOINT_COUNT_CHANGED_SYNC):
+            current = iface._read_int32()
+            target = iface._read_int32()
+            # ============================
+            # BEGIN ON CP COUNT
+            # ============================
+            # ============================
+            # END ON CP COUNT
+            # ============================
+            iface._respond_to_call(msgtype)
+        elif msgtype == int(MessageType.SC_LAP_COUNT_CHANGED_SYNC):
+            iface._read_int32()
+            iface._respond_to_call(msgtype)
+        elif msgtype == int(MessageType.SC_REQUESTED_FRAME_SYNC):
+            iface._respond_to_call(msgtype)
+        elif msgtype == int(MessageType.C_SHUTDOWN):
+            iface.close()
+        elif msgtype == int(MessageType.SC_ON_CONNECT_SYNC):
+            # for i in range(0, len(vcp), 1):
+            for i in range(0, 9000, 1):
+                iface.execute_command(
+                    f"add_trigger {vcp[i][0]-0.2} {vcp[i][1]-0.2} {vcp[i][2]-0.2} {vcp[i][0]+0.2} {vcp[i][1]+0.2} {vcp[i][2]+0.2}"
+                )
+            iface._respond_to_call(msgtype)
+        else:
+            pass
 
 
-while True:
-    msgtype = iface._read_int32()
-    # =============================================
-    #        READ INCOMING MESSAGES
-    # =============================================
-    if msgtype == int(MessageType.SC_RUN_STEP_SYNC):
-        _time = iface._read_int32()
-        # ============================
-        # BEGIN ON RUN STEP
-        # ============================
-
-        # ============================
-        # END ON RUN STEP
-        # ============================
-        iface._respond_to_call(msgtype)
-    elif msgtype == int(MessageType.SC_CHECKPOINT_COUNT_CHANGED_SYNC):
-        current = iface._read_int32()
-        target = iface._read_int32()
-        # ============================
-        # BEGIN ON CP COUNT
-        # ============================
-        # ============================
-        # END ON CP COUNT
-        # ============================
-        iface._respond_to_call(msgtype)
-    elif msgtype == int(MessageType.SC_LAP_COUNT_CHANGED_SYNC):
-        iface._read_int32()
-        iface._respond_to_call(msgtype)
-    elif msgtype == int(MessageType.SC_REQUESTED_FRAME_SYNC):
-        iface._respond_to_call(msgtype)
-    elif msgtype == int(MessageType.C_SHUTDOWN):
-        iface.close()
-    elif msgtype == int(MessageType.SC_ON_CONNECT_SYNC):
-        # for i in range(0, len(vcp), 1):
-        for i in range(0, 9000, 1):
-            iface.execute_command(
-                f"add_trigger {vcp[i][0]-0.2} {vcp[i][1]-0.2} {vcp[i][2]-0.2} {vcp[i][0]+0.2} {vcp[i][1]+0.2} {vcp[i][2]+0.2}"
-            )
-            # time.sleep(0.005)
-        iface._respond_to_call(msgtype)
-    else:
-        pass
+if __name__ == "__main__":
+    main()
