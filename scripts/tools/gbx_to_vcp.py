83748abe6f3acaab041259cc201302edc55b316d
pb4git <pb.pubpub@gmail.com>
2024-01-19 19:14:24 +0100

    Refactor gbx_to_vcp such that the .gbx file path may now be passed as an argument. Add a small script docstring at the top of the script.
    (cherry picked from commit 149d9c40579a7b50788cd2dc98cf1683f23ce522)

--- scripts/tools/gbx_to_vcp.py
+++ scripts/tools/gbx_to_vcp.py
@@ -1,3 +1,13 @@
+"""
+This script reads a .gbx file, and creates a list of Virtual CheckPoints (VCP) based on the best ghost found in that file.
+
+The .gbx file may either be a .challenge.gbx, or a .replay.gbx.
+
+The VCP file is saved in base_dir/maps/map.npy. It should typically be renamed manually after running this script.
+
+The distance between virtual checkpoints is currently 50cm (hardcoded).
+"""
+import argparse
 from pathlib import Path
 
 import numpy as np
@@ -6,40 +16,36 @@ from pygbx import Gbx, GbxType
 from trackmania_rl.geometry import extract_cp_distance_interval
 
 
-def gbx_to_vcp(gbx_path: str, base_dir):
-    gbx = Gbx(gbx_path)
+def gbx_to_raw_pos_list(gbx_path: Path):
+    """
+    Read a .gbx file, extract the raw positions of the best ghost included in that file.
+    """
+    gbx = Gbx(str(gbx_path))
     ghosts = gbx.get_classes_by_ids([GbxType.CTN_GHOST])
     assert len(ghosts) > 0, "The file does not contain any ghost."
-    ghost = min(ghosts, key=lambda ghost: ghost.cp_times[-1])
+    ghost = min(ghosts, key=lambda g: g.cp_times[-1])
     assert ghost.num_respawns == 0, "The ghost contains respawns"
-    print(ghost.race_time, f"ghost has {len(ghost.records)} records and {len(ghost.control_entries)} control entries")
     records_to_keep = round(ghost.race_time / 100)
+
+    print(ghost.race_time, f"ghost has {len(ghost.records)} records and {len(ghost.control_entries)} control entries")
     print("Keeping", records_to_keep, "out of", len(ghost.records), "records for a race time of", ghost.race_time / 1000)
+
     raw_positions_list = []
     for r in ghost.records[:records_to_keep]:
         raw_positions_list.append(np.array([r.position.x, r.position.y, r.position.z]))
+
+    return raw_positions_list
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("gbx_path", type=Path)
+    args = parser.parse_args()
+    base_dir = Path(__file__).resolve().parents[2]
+
+    raw_positions_list = gbx_to_raw_pos_list(args.gbx_path)
     _ = extract_cp_distance_interval(raw_positions_list, 0.5, base_dir)
 
 
-base_dir = Path(__file__).resolve().parents[1]
-
-"""
-EXTRACT VCP FROM A SINGLE GBX
-"""
-gbx_to_vcp("../D15-Endurance True One Lap No Cut_ .Wirtual(00'44''16).Replay.Gbx", base_dir)
-
-
-"""
-EXTRACT VCP FROM MULTIPLE GBX
-"""
-# replay_dir = Path(r"C:\Users\chopi\Documents\TrackMania\Tracks\Challenges\Official Maps\Green\replays")
-
-# for replay_file in replay_dir.iterdir():
-#     map_name = replay_file.name.split(".")[0]
-#     # gbx_to_vcp(str(replay_file), base_dir)
-#     # shutil.copyfile(base_dir / "maps" / "map.npy", base_dir / "maps" / f"{map_name}_0.5m_author.npy")
-
-"""
-LIST MAP NAMES
-"""
-# [replay_file.name.split('.')[0] for replay_file in replay_dir.iterdir()]
+if __name__ == "__main__":
+    main()
