ed0e9acc357ea8cdffe232fc156f2887687cb90a
pb4git <pb.pubpub@gmail.com>
2024-05-27 18:20:00 +0200

    Modify setup.py such that `pip install -e .` is sufficient to install the package (except for pytorch/cuda)

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+def read_requirements(filename):
+    with open(filename, 'r') as f:
+        requirements = f.read().splitlines()
+    return requirements
 
 setup(
-    name="trackmania_rl",
-    version="0.1.0",
+    name='linesight',
+    version='3.0.1',
+    description='Trackmania AI with reinforcement learning.',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.10.1,<3.12.0',
+    install_requires=read_requirements('requirements_pip.txt') + read_requirements('requirements_conda.txt'),
     packages=find_packages(include=["trackmania_rl", "config_files"]),
 )
