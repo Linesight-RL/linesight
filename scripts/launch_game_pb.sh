e157b0c30bd3a1bd9a6e75a05d73f46e1a392a9d
pb4git <pb.pubpub@gmail.com>
2024-10-03 09:15:45 +0200

    Personal update: Updated launch_game_pb.sh

--- scripts/launch_game_pb.sh
+++ scripts/launch_game_pb.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cd ~/tmnf_steam/
-
 # Basic launch command:
 #exec wine "./TMInterfaceTesting.exe" "/configstring=set custom_port $1" #WINEFSYNC=1 STAGING_SHARED_MEMORY=1 STAGING_WRITECOPY=1
 
 # Gamemode launch command: may not work on all systems
 # exec gamemoderun wine "./TMInterfaceTesting.exe" "/configstring=set custom_port $1" #WINEFSYNC=1 STAGING_SHARED_MEMORY=1 STAGING_WRITECOPY=1
 
-exec gamemoderun wine ~/tmnf_steam/TMLoader-win32/TMLoader.exe run TmForever "default" /configstring="set custom_port $1"
+exec gamemoderun wine ~/tmnf/TMLoader-1.0.1-win32/TMLoader.exe run TmForever "default" /configstring="set custom_port $1"
