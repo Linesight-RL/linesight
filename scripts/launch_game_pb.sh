4f9abd325b36e2251409897144a9e94e0cdfa183
Marcus <cufbas@gmail.com>
2025-03-20 15:54:55 +0100

    working launch script

--- scripts/launch_game_pb.sh
+++ scripts/launch_game_pb.sh
@@ -6,4 +6,4 @@
 # Gamemode launch command: may not work on all systems
 # exec gamemoderun wine "./TMInterfaceTesting.exe" "/configstring=set custom_port $1" #WINEFSYNC=1 STAGING_SHARED_MEMORY=1 STAGING_WRITECOPY=1
 
-exec gamemoderun wine ~/tmnf/TMLoader-1.0.1-win32/TMLoader.exe run TmForever "default" /configstring="set custom_port $1"
+exec gamemoderun wine ./TMloader/TMLoader.exe run TmForever "default" /configstring="set custom_port $1"
